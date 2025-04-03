1b23f89142db13acde0531a0301e70675e147e7b
s1ddiq <siddiq.nasheed@gmail.com>
2025-04-03 02:49:34 +0200

    UI rework and API service change

--- api/api.ts
+++ api/api.ts
@@ -5,14 +5,9 @@ const API_KEY = process.env.NEXT_PUBLIC_QUOTES_API_KEY!;
 
 function handleError(message: string, error: any) {console.log(`${message}: ${error}`)}
 
-export const fetchQuotes = async () => {
+export const fetchQuote = async () => {
     try {
-        const response = await fetch('https://api.api-ninjas.com/v1/quotes', {
-            method: 'GET',
-            headers: {
-                'X-Api-Key': process.env.NEXT_PUBLIC_QUOTES_API_KEY!, // API key
-            }
-        });
+        const response = await fetch('https://qapi.vercel.app/api/random');
 
         if (!response.ok) {
             throw new Error('Network response was not ok');
@@ -21,6 +16,28 @@ export const fetchQuotes = async () => {
         const data = await response.json();
         return data; // You can inspect this if it's an array or object.
     } catch (error) {
-        console.error('Error fetching quotes:', error);
+        handleError('Error fetching quotes:', error);
     }
 };
+
+export const fetchAllQuotes = async () => {
+    try {
+        const response = await fetch('https://qapi.vercel.app/api/quotes');
+
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+
+        const data = await response.json();
+        return data; // You can inspect this if it's an array or object.
+    } catch (error) {
+        handleError('Error fetching quotes:', error);
+    }
+};
+
+{/*const response = await fetch('https://zenquotes.io/api/quotes/', {
+            method: 'GET',
+            headers: {
+                'X-Api-Key': process.env.NEXT_PUBLIC_QUOTES_API_KEY!, // API key
+            }
+        }); */}
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,31 +1,66 @@
-'use client'
-import { fetchQuotes } from '@/api/api';
-import Image from 'next/image';
-import React, { useEffect, useState } from 'react'
+"use client";
+import { fetchQuote, fetchAllQuotes } from "@/api/api";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useState } from "react";
 
 const QuotesPage = () => {
-  const [quotes, setQuotes] = useState<{ quote: string, author: string, category: string }[]>([]);
-
+  const [quote, setQuote] = useState<
+    { quote: string; author: string; id: number }[]
+  >([]);
+  const [quotes, setQuotes] = useState<
+    { quote: string; author: string; id: number }[]
+  >([]);
   useEffect(() => {
     const func = async () => {
-        const response = await fetchQuotes();
-        setQuotes(response);
-        console.log(response);
-    }
-    func()
-  }, [])
+      const quote = await fetchQuote();
+      setQuote(quote);
+      const quotes = await fetchAllQuotes();
+      setQuotes(quotes);
+      console.log(quotes);
+    };
+    func();
+  }, []);
 
   return (
-    <div className='flex justify-center items-center w-full h-screen flex-col'>
-      {quotes.map((quote, index) => (
-        <div key={index} className='w-full px-24 relative'>
-            <p className='text-xl text-center'>{quote.quote}</p>
-            <p className='float-right text-2xl'> - {quote.author}</p>
-            {/* <p className='opacity-80'>{quote.category}</p> */}
-        </div>
-      ))}
+    <div className="pt-16 mb-32">
+      <h1 className="text-6xl text-center opacity-10 font-bold">
+        Stay up to Quote :)
+      </h1>
+      <div className="flex justify-center items-center w-full h-screen flex-row flex-wrap lg:px-64 md:px-32 sm:px-24 px-16 gap-8 pt-16">
+        {quotes.map((quote, index) => (
+          <Link key={index} href={`/quote/${quote.id}`}>
+            <div
+              className="
+            w-64 min-h-32 h-auto
+            bg-transparent
+            border 
+            border-px
+            border-[#262629ff]
+            rounded-xl p-4
+            relative
+            hover:-translate-y-2
+            cursor-pointer
+            transition-discrete
+            transition-all
+            duration-300
+            "
+            >
+              <div>
+                <p className="font-light text-sm">{quote.quote}</p>
+                <p className="text-sm font-semibold absolute bottom-2 right-4">
+                  {quote.author}
+                </p>
+                <p className="font-semibold text-base absolute bottom-2">
+                  {quote.id}
+                </p>
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default QuotesPage
+export default QuotesPage;
